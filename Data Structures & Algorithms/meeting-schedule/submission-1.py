9865a85b81d1d499f602c255345ec6d3815d64ca
utk <utkarsh.ankit09@gmail.com>
2026-04-19 08:51:37 -0700

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,17 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x:x.start)
+        i=0
+        while i<len(intervals)-1:
+            if intervals[i].end > intervals[i+1].start:
+                return False
+            i+=1
+        return True
