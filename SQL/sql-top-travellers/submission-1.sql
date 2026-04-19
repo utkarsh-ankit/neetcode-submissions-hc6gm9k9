3830627dc7589bde5a9cd92f448478708c280539
utk <utkarsh.ankit09@gmail.com>
2026-04-18 21:28:28 -0700

    Add: sql-top-travellers - submission-1

---
+++ SQL/sql-top-travellers/submission-1.sql
@@ -0,0 +1,7 @@
+-- Write your query below
+SELECT u.name, COALESCE(SUM(r.distance),0) AS travelled_distance
+FROM users u
+LEFT JOIN rides r 
+ON u.id=r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance desc, u.name ASC
