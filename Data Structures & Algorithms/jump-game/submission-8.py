3217ad84aabe9802057a60a32a4f000060d15118
utk <utkarsh.ankit09@gmail.com>
2026-04-28 16:24:53 -0700

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        reachable = 0
+        for i in range(len(nums)):
+            if i > reachable:
+                return False
+            reachable = max(reachable, i + nums[i])
+            if reachable >= len(nums) - 1:
+                return True
+        return False
