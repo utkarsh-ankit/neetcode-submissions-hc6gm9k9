fb0a8a507e937bd2441fd75fced096963d5ad1ca
utk <utkarsh.ankit09@gmail.com>
2026-04-30 09:12:35 -0700

    Add: single-number - submission-3

---
+++ Data Structures & Algorithms/single-number/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # c={i:nums.count(i) for i in nums}
+        # for i,v in c.items():
+        #     if v==1:
+        #         return i
+
+        res=0
+        
+        for i in nums:
+            res=i^res
+        return res
+        
