14f6db825a77a1a5e343984b88117cda49777c22
utk <utkarsh.ankit09@gmail.com>
2026-05-22 08:48:03 -0700

    Add: lfu-cache - submission-0

---
+++ Data Structures & Algorithms/lfu-cache/submission-0.py
@@ -0,0 +1,101 @@
+class ListNode:
+
+    def __init__(self, key, val):
+        #we need to maintain a counter
+        #along with the object, that check frequency and also the regency
+        self.key=key
+        self.val=val
+        self.freq=1
+        self.prev=None
+        self.next=None
+
+class LinkedList:
+
+    def __init__(self):
+        self.left=ListNode(0,0)
+        self.right=ListNode(0,0)
+        self.left.next=self.right
+        self.right.prev=self.left
+        self.size=0
+
+    def length(self):
+        return self.size
+
+    def pushRight(self,node):
+        prev=self.right.prev
+        prev.next=node
+        node.prev=prev
+        node.next=self.right
+        self.right.prev=node
+        self.size+=1
+
+    def pop(self,node):
+        prev,next=node.prev,node.next
+        prev.next=next
+        next.prev=prev
+        node.prev=None
+        node.next=None
+        self.size-=1
+
+    def popLeft(self):
+        if self.length()==0:
+            return None
+        node=self.left.next
+        self.pop(node)
+        return node
+
+class LFUCache:
+
+    def __init__(self, capacity: int):
+        #we need to maintain a counter
+        #along with the object, that check frequency and also the regency
+        self.cap=capacity
+        self.lfuCnt=0
+        self.nodeMap={}
+        self.listMap=defaultdict(LinkedList)
+
+    def counter(self,node):
+        cnt=node.freq
+        self.listMap[cnt].pop(node)
+
+        if cnt==self.lfuCnt and self.listMap[cnt].length()==0:
+            self.lfuCnt+=1
+
+        node.freq+=1
+        self.listMap[node.freq].pushRight(node)
+
+
+    def get(self, key: int) -> int:
+        #first frequncy, then regency
+        if key not in self.nodeMap:
+            return -1
+        node=self.nodeMap[key]
+        self.counter(node)
+        return node.val
+        
+
+    def put(self, key: int, value: int) -> None:
+        if self.cap==0:
+            return
+
+        if key in self.nodeMap:
+            node=self.nodeMap[key]
+            node.val=value
+            self.counter(node)
+            return
+        
+        if len(self.nodeMap)==self.cap:
+            node=self.listMap[self.lfuCnt].popLeft()
+            self.nodeMap.pop(node.key)
+
+        node=ListNode(key,value)
+        self.nodeMap[key]=node
+        self.listMap[1].pushRight(node)
+        self.lfuCnt=1
+        
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
