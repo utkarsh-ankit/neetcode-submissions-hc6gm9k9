615d714f53da33b02474b18b903aba0a325fff69
utk <utkarsh.ankit09@gmail.com>
2026-06-07 09:45:58 -0700

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
+            return False
+
+        s1_count=Counter(s1)
+
+        w=Counter(s2[:len(s1)])
+
+        if w==s1_count:
+            return True
+
+        for i in range(len(s1), len(s2)):
+            l=s2[i-len(s1)]
+            r=s2[i]
+
+            w[l]-=1
+            w[r]+=1
+
+            if w==s1_count:
+                return True
+                
+        return False
+
