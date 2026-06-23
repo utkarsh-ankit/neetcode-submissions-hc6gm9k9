8b4fa5c9e1961e782df1f450e0197e496849f4b9
utk <utkarsh.ankit09@gmail.com>
2026-06-22 23:44:22 -0700

    Add: maximum-subarray - submission-10

---
+++ Data Structures & Algorithms/maximum-subarray/submission-10.py
@@ -0,0 +1,18 @@
+#The concept is, if all are negative, the max will be not the sum but the min nagetive mumber.
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        summ=float("-inf")
+        current=0
+
+        for i in range(len(nums)):
+            current+=nums[i]
+            summ=max(summ, current)
+            if current<0:
+                current=0
+
+        return summ
+
+
+
+        
