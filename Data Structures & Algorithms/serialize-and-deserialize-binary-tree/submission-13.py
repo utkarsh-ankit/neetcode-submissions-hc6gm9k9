ed95aa67a29f6a9adb4f6f919011e5e6ec3c7acf
utk <utkarsh.ankit09@gmail.com>
2026-07-19 01:26:46 -0700

    Add: serialize-and-deserialize-binary-tree - submission-13

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-13.py
@@ -0,0 +1,76 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        # result=[]
+
+        # def dfs(node):
+        #     if not node:
+        #         result.append('n')
+        #         return
+        #     result.append(str(node.val))
+        #     dfs(node.left)
+        #     dfs(node.right)
+
+        # dfs(root)
+
+        # return ",".join(result)
+
+        result=[]
+
+        def dfs(node):
+            if not node:
+                result.append('n')
+                return
+            result.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ",".join(result)
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        # vals=data.split(",")
+        # self.i=0
+
+        # def dfs():
+        #     if vals[self.i]=="n":
+        #         self.i+=1
+        #         return None
+        #     node=TreeNode(int(vals[self.i]))
+        #     self.i+=1
+
+        #     node.left=dfs()
+        #     node.right=dfs()
+
+        #     return node
+
+        # return dfs()
+
+        vals=data.split(",")
+        self.i=0
+
+        def dfs():
+            if vals[self.i]=="n":
+                self.i+=1
+                return None
+            node=TreeNode(int(vals[self.i]))
+            self.i+=1
+
+            node.left=dfs()
+            node.right=dfs()
+
+            return node
+
+        return dfs()
+
+
+
