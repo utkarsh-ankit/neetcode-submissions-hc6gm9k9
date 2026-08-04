bd0e2c719f3810fc1d501fd3cf7e4962a8ecd434
utk <utkarsh.ankit09@gmail.com>
2026-08-03 21:00:16 -0700

    Add: two-integer-sum - submission-17

---
+++ Data Structures & Algorithms/two-integer-sum/submission-17.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # for i in range(len(nums)):
+        #         a=nums[i]
+        #         b=target-a
+        #         if b in nums and nums.index(b)!=i:
+        #             return [i,nums.index(b)]
+
+
+        # hmap={nums[i]:i for i in range(len(nums))}
+
+        # for i in range(len(nums)):
+        #     d=target-nums[i]
+        #     if d in hmap and hmap[d]!=i:
+        #         return [i,hmap[d]]
+
+        hmap={nums[i]:i for i in range(len(nums))}
+
+        for i in range(len(nums)):
+            d=target-nums[i]
+            if d in hmap and hmap[d]!=i:
+                return [i,hmap[d]]
+                
+            
