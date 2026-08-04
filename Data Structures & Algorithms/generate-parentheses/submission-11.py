0e05bd7c9415e882f0b3bae8dc9ba49d049a72df
utk <utkarsh.ankit09@gmail.com>
2026-08-03 23:21:06 -0700

    Add: generate-parentheses - submission-11

---
+++ Data Structures & Algorithms/generate-parentheses/submission-11.py
@@ -0,0 +1,59 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # result=[]
+
+        # def backtrack(current,op,cl):
+        #     if len(current)==2*n:
+        #         result.append(current)
+
+        #     if op<n:
+        #         backtrack(current+"(",op+1,cl)
+        #     if cl<op:
+        #         backtrack(current+")",op,cl+1)
+
+        # backtrack("",0,0)
+
+        # return result
+
+#---
+
+        # result=[]
+
+        # def backtrack(string_,op,cl):
+        #     if len(string_)==2*n:
+        #         result.append(string_)
+        #         return
+        #     if op<n:
+        #         backtrack(string_+"(", op+1, cl)
+        #     if cl<op:
+        #         backtrack(string_+")", op, cl+1)
+
+        # backtrack("",0,0)
+
+        # return result
+
+        result=[]
+
+        def backtrack(string,open,close):
+            if len(string)==2*n:
+                result.append(string)
+            if open<n:
+                backtrack(string+"(",open+1,close)
+            if close<open:
+                backtrack(string+")",open,close+1)
+        backtrack("",0,0)
+        return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
