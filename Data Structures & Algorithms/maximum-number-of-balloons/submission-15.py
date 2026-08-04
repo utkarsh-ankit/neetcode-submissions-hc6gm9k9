5ad158b89cb0719245d238e5d498944b7236b274
utk <utkarsh.ankit09@gmail.com>
2026-08-03 23:39:43 -0700

    Add: maximum-number-of-balloons - submission-15

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-15.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        # balloon=Counter('balloon')
+        # c=Counter(text)
+
+        # res=float("inf")
+
+        # for i in balloon:
+        #     res=min(res,c[i]//balloon[i])
+        # return res
+
+        balloon=Counter('balloon')
+        c=Counter(text)
+
+        res=float("inf")
+
+        for i in balloon:
+            res=min(res,c[i]//balloon[i])
+        return res
+
+        
